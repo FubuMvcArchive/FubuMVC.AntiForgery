5ec794be947d4a1bc78227dce38cc7618cc787be
jeremydmiller <jeremydmiller@yahoo.com>
2012-11-12 09:01:47 -0600

    imported the IFubuPage extensions for anti forgery

---
+++ src/FubuMVC.AntiForgery/AntiForgeryTokenFubuPageExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FubuMVC.Core.View;
+using HtmlTags;
+using HtmlTags.Extended.Attributes;
+
+namespace FubuMVC.AntiForgery
+{
+    public static class AntiForgeryTokenFubuPageExtensions
+    {
+        public static HtmlTag AntiForgeryToken(this IFubuPage page, string salt)
+        {
+            return AntiForgeryToken(page, salt, null, null);
+        }
+
+        public static HtmlTag AntiForgeryToken(this IFubuPage page, string salt, string path, string domain)
+        {
+            var antiForgeryService = page.Get<IAntiForgeryService>();
+            AntiForgeryData cookieToken = antiForgeryService.SetCookieToken(path, domain);
+            FormToken formToken = antiForgeryService.GetFormToken(cookieToken, salt);
+
+            return new HiddenTag().Name(formToken.Name).Value(formToken.TokenString);
+        }
+    }
+}
